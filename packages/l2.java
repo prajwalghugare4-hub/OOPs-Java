e9a8988ed14020ab66ec237d8832b89f41f05004
prajwalghugare4-hub <prajwalghugare4@gmail.com>
2026-08-21 22:36:51 +0530

    Lecture 2 completed

--- packages/l2.java
+++ packages/l2.java
@@ -1,18 +1,38 @@
  // Static is keyword which belongs to class funstion and which is indpendent on objects
  // Non Static is dependent on objects and can access inside the object funtion only
+ import java.util.Scanner;
 class B 
 {  
      
     public static void main(String[] args) {
         // To Access the non static in static either non static should get a class or make it static 
         // Here B is class obj is new object
-        B obj = new B();
-        obj.greeting();
-        Student s4=new Student();
-        System.out.println(s4.name);
+        Employee Yash =new Employee();
+        System.out.println(Yash.name);
+        System.out.println(Yash.salary);
+        System.out.println(Yash.country);
+        
+        // System - class
+        // out - variable
+        // print - method
     }
 
-    void greeting(){
+    static void greeting(){
         System.out.println("Hello World!");
+    }
+}
+
+class Employee {
+
+    final String country="India";
+    String name;
+    int salary;
+    Employee(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name:");
+        name=sc.next();
+        System.out.print("Salary:");
+        salary=sc.nextInt();
+        sc.close();
     }
 }
